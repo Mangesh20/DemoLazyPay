19d554f2230d2b45e2db77f2cfb3c94151682453
Mangesh Tekale <mangesht@cybage.com>
2018-08-13 17:27:57 +0530

    Initial Commit

---
+++ DemoLazyPay/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  DemoLazyPay
+//
+//  Created by Mangesh Tekale on 8/9/18.
+//  Copyright © 2018 Mangesh Tekale. All rights reserved.
+//
+
+import UIKit
+
+import CitrusPay
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+
+
